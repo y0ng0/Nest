4983aa35a15ce0e1bb782d72d271230aaaa0d052
s_yuyongxuan1 <s_yuyongxuan1@wps.cn>
2024-04-02 15:09:06 +0800

    feat: 上传图片接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FileUploadController } from './file-upload/file-upload.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, FileUploadController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/file-upload/file-upload.controller.ts
@@ -0,0 +1,46 @@
+// file-upload.controller.ts
+
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  BadRequestException,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('file')
+export class FileUploadController {
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './imgs', // 上传文件存储的目录
+        filename: (req, file, cb) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          return cb(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          return cb(
+            new BadRequestException('Only image files are allowed!'),
+            false,
+          );
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  async uploadFile(@UploadedFile() file) {
+    return {
+      originalname: file.originalname,
+      filename: file.filename,
+    };
+  }
+}
